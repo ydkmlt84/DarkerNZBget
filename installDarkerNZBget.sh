3882ce675dfbd3e2de275d1cc313ef384cd44b79
ydkmlt84 <justin@turnedaround.net>
2018-03-26 19:22:25 -0700

    nice Linux install script made by @christronyxyocum. Thanks.

--- installDarkerNZBget.sh
+++ installDarkerNZBget.sh
@@ -1,45 +1,71 @@
-#!/bin/bash
-#The below <backupfile> is the name of the backup for your stock CSS. Change it if you'd like. :)
-backupfile="($(date +"%Y%m%d-%I:%M%p"))-stylebackup.css"
-#This is the name of the CSS file. Do not change.
-DarkerNZBgetCSS="nzbget_custom_darkblue.css"
-#Change the <nzbgetdir> location below to where your nzbget is installed to.
-nzbgetdir="/opt/nzbget"
-#The start of actually doing something with the script
-cat Misc/art.txt
-if [ -d "$nzbgetdir"/webui ];
-  then
-    sudo mkdir -p "$nzbgetdir"/webui/CSSbackups
-  else
-    echo "There was a problem with creating the backup directory. Please make sure the nzbgetdir location is correct. Exiting"
-      exit
+#!/usr/bin/env bash
+#
+# DarkerNZBGet theme installer
+# v1.0.0 - 03/26/2018 - The original
+# Tronyx
+#
+# Determine if user is root and, if not, tell them to use sudo
+if [ "$EUID" -ne 0 ]; then
+  echo "Please run as root or use sudo when executing this script..."
+  exit
 fi
-sleep 2
-sudo cp "$nzbgetdir"/webui/style.css "$nzbgetdir"/webui/CSSbackups/"$backupfile"
-if [ -f "$nzbgetdir"/webui/CSSbackups/"$backupfile" ];
-  then
-    echo "Backup of stock CSS created at "$nzbgetdir"/webui/CSSbackups/"$backupfile""
-  else
-    echo "Could not create a backup of the stock CSS file. Exiting"
-        sleep 2
-    exit
-fi
-sleep 3
 
-#  We verified the backup was created in a step before so we will remove the stock CSS file and
-#  prepare to copy the new file to the stockcss location.
-echo "Inserting the DarkerNZBget CSS now..."
-sudo rm "$nzbgetdir"/webui/style.css
-sudo cp "$DarkerNZBgetCSS" "$nzbgetdir"/webui/style.css
-sleep 3
+# Determine which Package Manager to use
+echo "Gathering some information and setting some variables..."
+declare -A osInfo;
+osInfo[/etc/redhat-release]='yum -y -q'
+osInfo[/etc/arch-release]=pacman
+osInfo[/etc/gentoo-release]=emerge
+osInfo[/etc/SuSE-release]=zypp
+osInfo[/etc/debian_version]='apt-get -y -qq'
 
-#Checking to make sure all is well. Almost done.
-if [ -f "$nzbgetdir"/webui/style.css ];
-  then
-      echo "DarkerNZBget has been installed. Now to clean up the mess a little bit."
-  else
-      echo "We got so close...we had trouble moving the new theme into "$nzbgetdir"/webui. Exiting."
-        exit
+for f in ${!osInfo[@]}
+do
+  if [[ -f $f ]];then
+    packageManager=${osInfo[$f]}
+  fi
+done
+
+# Determine Nzbget install location and set it as a variable
+nzbgetDir=$(locate nzbget |grep conf |head -1 |rev |cut -c12-50 |rev)
+
+# Make sure unzip and wget are installed
+echo "Making sure unzip and wget are installed..."
+${packageManager} install unzip wget
+
+# Grab the Darker Nzbget theme CSS archive and extract it
+echo "Downloading and extracting the Darker Nzbget theme CSS archive..."
+wget -q -O /tmp/DarkerNZBget.zip https://github.com/ydkmlt84/DarkerNZBget/archive/develop.zip
+unzip -qq -o /tmp/DarkerNZBget.zip -d /tmp/
+
+# Display banner
+cat /tmp/DarkerNZBget-develop/Misc/art.txt
+
+# Create backup dir and make a backup of the existing style.css file
+echo "Backing up the original style.css file..."
+if [ ! -d "${nzbgetDir}webui/backup" ]; then
+  mkdir "${nzbgetDir}webui/backup"
+  yes |cp -rf "${nzbgetDir}webui/style.css" "${nzbgetDir}webui/backup/style.css.original"
+else
+  :
 fi
-sleep 4
-echo "Done...'fingers crossed' you should be able to see the custom CSS now. Refresh the page."
+
+# Install the DarkerNZBget CSS
+echo "Installing the custom CSS file..."
+if [ ! -f "${nzbgetDir}webui/backup/style.css.original" ]; then
+  echo "Whoops.. Somehow the stock style.css file didn't get backed up..."
+  echo "Let's try that again..."
+  yes |cp -rf "${nzbgetDir}webui/style.css" "${nzbgetDir}webui/backup/style.css.original"
+  echo "Installing the custom CSS file...again..."
+  yes |cp -rf /tmp/DarkerNZBget-develop/nzbget_custom_darkblue.css "${nzbgetDir}webui/style.css"
+else
+  yes |cp -rf /tmp/DarkerNZBget-develop/nzbget_custom_darkblue.css "${nzbgetDir}webui/style.css"
+fi
+
+# Verify the DarkerNZBget CSS has been installed and display corresponding message(s)
+if grep -q DarkerNZBGet "${nzbgetDir}webui/style.css"; then
+  echo "The DarkerNZBget theme has been successfully installed!"
+  echo "Please refresh your browser window/tab to see the new theme."
+else
+  echo "The DarkerNZBget theme has NOT been installed!"
+fi
